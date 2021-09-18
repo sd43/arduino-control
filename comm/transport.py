f5488b3055fb8679539c69b9eca3362976cf82d5
-- <-->
2021-09-18 11:04:26 +0530

    serial port

--- comm/transport.py
+++ comm/transport.py
@@ -1,5 +1,7 @@
-import socket
 import logging
+
+import socket
+import serial, serial.tools.list_ports
 
 class Request():
     def __init__(self, cmd, args, id=None):
@@ -30,13 +32,20 @@ class Response():
         return '{:12} {}({})'.format(self.id or '', self.status, resstr)
 
 class TcpTransport():
-    def __init__(self, host, port):
-        self.server = (host, port)
+    def __init__(self, host=None, port=None):
+        if host is not None and port is not None:
+            self.server = (host, port)
         self.sock = None
         self.sockR, self.sockW = None, None
 
     def isConnected(self):
         return self.sock != None
+
+    def setAddress(self, host, port):
+        if self.isConnected():
+            raise Exception('cannot set TCP address if already connected')
+
+        self.server = (host, port)
 
     def connect(self):
         try:
@@ -98,6 +107,73 @@ class TcpTransport():
         try:
             self.sockW.write(text + '\n');
             self.sockW.flush()
+        except Exception as e:
+            logging.error("failed to send data: {}".format(str(e)))
+            self.disconnect()
+            raise(e)
+
+class SerialTransport():
+    def __init__(self):
+        pass
+
+    def setSerialPort(self, serialPortDevice, baudRate):
+        self.device = serialPortDevice
+        self.baudRate = baudRate
+
+    def getSerialPorts():
+        ports = {port.device: port.name for port in serial.tools.list_ports.comports()}
+        return ports
+
+    def isConnected(self):
+        pass
+
+    def ping(self):
+        req = Request(id='connect',cmd='ping')
+        self.send(req)
+
+    def connect(self):
+        self.serial = serial.Serial(self.device, self.baudRate, timeout=5)
+
+        try:
+            self.ping()
+        except Exception as e:
+            logging.error("failed to connect to server {}".format(self.server))
+            raise(e)
+        
+    def disconnect(self):
+        if self.isConnected():
+            try:
+                self.serial.close()
+            finally:
+                return True
+        return False
+
+    def recv(self):
+        try:
+            text = self.serial.readline()
+        except Exception as e:
+            logging.error("failed to read data: {}".format(str(e)))
+            self.disconnect()
+            raise(e)
+
+        text = text.rstrip('\n\r')
+        logging.debug("recv {}".format(text))
+
+        parts = text.split(':')
+        if len(parts) != 3:
+            raise Exception('bad message format from server: "{}"'.format(output))
+        commandId, status, results = parts
+        results = results.split(',')
+
+        return Response(id=commandId, status=status, results=results)
+
+    def send(self, request):
+        argstr = ','.join([str(arg) for arg in request.args])
+        text = '{}:{}:{}\n'.format(request.id, request.cmd, argstr)
+        logging.debug("send {}".format(text))
+
+        try:
+            self.serial.write(text.encode())
         except Exception as e:
             logging.error("failed to send data: {}".format(str(e)))
             self.disconnect()
--- gui/connection_bar.py
+++ gui/connection_bar.py
@@ -1,12 +1,22 @@
 import logging
 
-from comm.transport import TcpTransport
+from comm.transport import TcpTransport, SerialTransport
 
 from PyQt5.QtWidgets import *
 
-class ConnectionBar(QWidget):
+class ConnectionBar(QTabWidget):
     def __init__(self, controller, parent=None):
         super(ConnectionBar, self).__init__(parent)
+
+        self.tcpTab = TcpConnectionBar(controller)
+        self.serialTab = SerialConnectionBar(controller)
+
+        self.addTab(self.tcpTab, "TCP")
+        self.addTab(self.serialTab, "Serial")
+
+class TcpConnectionBar(QWidget):
+    def __init__(self, controller, parent=None):
+        super(TcpConnectionBar, self).__init__(parent)
 
         self.controller = controller
 
@@ -36,7 +46,9 @@ class ConnectionBar(QWidget):
             host = addrParts[0]
             try:
                 port = int(addrParts[1])
-                transport = TcpTransport(host, port)
+                transport = TcpTransport()
+                transport.setAddress(host, port)
+
                 self.controller.setTransport(transport)
                 self.controller.connect()
 
@@ -55,6 +67,82 @@ class ConnectionBar(QWidget):
             self.showError('Failed to disconnect: ' + str(e))
 
         self.wServer.setEnabled(True)
+        self.wDisconnect.hide()
+        self.wConnect.show()
+
+    def showError(self, message):
+        QMessageBox.critical(self, 'Error', message)
+
+class SerialConnectionBar(QWidget):
+    def __init__(self, controller, parent=None):
+        super(SerialConnectionBar, self).__init__(parent)
+
+        self.controller = controller
+        self.serialPorts = {}
+
+        layout = QHBoxLayout()
+        self.wSerialPort = QComboBox()
+        self.updateSerialPortList()
+        self.baudRates = [2400, 4800, 9600, 14400, 19200, 2800, 38400, 57600, 76800, 115200]
+        self.wBaudRate = QComboBox()
+        for b in self.baudRates:
+            self.wBaudRate.addItem(str(b))
+
+        self.wConnect = QPushButton('Connect')
+        self.wDisconnect = QPushButton('Disconnect')
+        self.wDisconnect.hide()
+
+        self.wConnect.clicked.connect(self.onConnect)
+        self.wDisconnect.clicked.connect(self.onDisconnect)
+
+        layout.addWidget(QLabel('Serial Port:'))
+        layout.addWidget(self.wSerialPort)
+        layout.addWidget(QLabel('Baud Rate:'))
+        layout.addWidget(self.wBaudRate)
+        layout.addWidget(self.wConnect)
+        layout.addWidget(self.wDisconnect)
+
+        self.setLayout(layout)
+
+    def updateSerialPortList(self):
+        serialPorts = SerialTransport.getSerialPorts()
+        self.serialPorts = sorted(
+                [ {'name': serialPorts[port], 'device': port } for port in serialPorts ],
+                key=lambda port: port['name']
+        )
+
+        self.wSerialPort.clear()
+        for port in self.serialPorts:
+            self.wSerialPort.addItem(port['name'])
+
+    def onConnect(self):
+        logging.info("connecting to server...")
+
+        serialPort = self.serialPorts[self.wSerialPort.currentIndex()]
+        baudRate = self.baudRates[self.wBaudRate.currentIndex()]
+
+        try:
+            transport = SerialTransport()
+            transport.setSerialPort(serialPort['device'], baudRate)
+
+            self.controller.setTransport(transport)
+            self.controller.connect()
+
+            self.wSerialPort.setEnabled(False)
+            self.wBaudRate.setEnabled(False)
+            self.wDisconnect.show()
+            self.wConnect.hide()
+        except Exception as e:
+            self.showError('Failed to connect to server: ' + str(e))
+
+    def onDisconnect(self):
+        try:
+            self.controller.disconnect()
+        except Exception as e:
+            self.showError('Failed to disconnect: ' + str(e))
+
+        self.wSerialPort.setEnabled(True)
+        self.wBaudRate.setEnabled(True)
         self.wDisconnect.hide()
         self.wConnect.show()
 
